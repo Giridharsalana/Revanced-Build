7e3b7709032956ee32bc03deff71cab3373e81ca
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2022-10-29 09:47:31 +0000

    Update

--- Revanced.sh
+++ Revanced.sh
@@ -6,4 +6,6 @@ chmod +x patch.sh
 export additional_args="-i enable-wide-searchbar -e hide-shorts-button -e disable-create-button -e hide-cast-button -e hide-autoplay-button -e disable-fullscreen-panels -e old-quality-layout -e hide-infocard-suggestions -e enable-wide-searchbar"
 sudo apt install openjdk-17-jdk openjdk-17-jre -y 
 ./patch.sh
+cp *-non-root.apk ../revanced.apk
+curl -T ../revanced.apk temp.sh
 rm -rf ../Revanced
