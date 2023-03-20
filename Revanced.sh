734f9167fc94a859b9347b3d4daccb555c3ecda0
Giridhar Salana <37552476+Giridharsalana@users.noreply.github.com>
2023-03-20 12:22:30 +0530

    Update Revanced.sh

--- Revanced.sh
+++ Revanced.sh
@@ -4,11 +4,7 @@ mkdir Revanced
 cd Revanced
 wget https://raw.githubusercontent.com/XDream8/revanced-creator/main/patch.sh
 chmod +x patch.sh
-#export additional_args="-i enable-wide-searchbar -e hide-shorts-button -e disable-create-button -e hide-cast-button -e hide-autoplay-button -e disable-fullscreen-panels -e old-quality-layout -e hide-infocard-suggestions -e enable-wide-searchbar"
-#export additional_args="-e return-youtube-dislike"
-#sudo apt update && sudo apt upgrade -y
-#sudo apt install openjdk-17-jdk openjdk-17-jre -y 
+export additional_args="-e return-youtube-dislike"
 ./patch.sh
 cp *-non-root.apk ../revanced.apk
-
-#rm -rf ../Revanced
+rm -rf ../Revanced
